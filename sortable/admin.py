149497df31000ee67ba612d224e3f529aed624a4
claudiob <claudiob@gmail.com>
2011-04-27 16:58:16 -0700

    Requiring external jquery and jquery-UI as django-admin does not always have them

--- sortable/admin.py
+++ sortable/admin.py
@@ -4,6 +4,7 @@ class SortableAdmin(admin.ModelAdmin):
     # Make instances reorderable
     list_editable = ('position',)
     list_display = ('position', )
-    class Media: js = ('js/admin_list_reorder.js',)
-
-
+    class Media: js = (
+        'http://ajax.googleapis.com/ajax/libs/jquery/1.5/jquery.min.js',
+        'http://ajax.googleapis.com/ajax/libs/jqueryui/1.8/jquery-ui.min.js',
+        'js/django-admin-sortable.js',)
