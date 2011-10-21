9b02e114b04845f140c1b635ce03bf34f06070a5
David Isaacson <dave.isaacson@gmail.com>
2011-10-21 16:56:07 -0700

    Removed default to cause new items to be added to bottom of sort; used more appropriate field type

--- sortable/models.py
+++ sortable/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Sortable(models.Model):
     # Make instances reorderable
-    position = models.IntegerField(default=0)
+    position = models.PositiveSmallIntegerField()
 
     def save(self, *args, **kwargs):
         model = self.__class__
