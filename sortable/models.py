efb73bcd97747dbd5f17d504c2a679adb2c5e357
Leandro Ferreira <durang@gmail.com>
2014-05-30 10:33:43 -0300

    fixed sortable model

--- sortable/models.py
+++ sortable/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Sortable(models.Model):
     # Make instances reorderable
-    position = models.IntegerField(default=0)
+    position = models.IntegerField(blank=True)
 
     def save(self, *args, **kwargs):
         model = self.__class__
@@ -13,6 +13,14 @@ class Sortable(models.Model):
                 self.position = last.position + 1
             except IndexError: self.position = 0
         return super(Sortable, self).save(*args, **kwargs)
+
+    def update_indexes(self):
+        model = self.__class__
+        items = model.objects.order_by('position')
+        for index in range(len(items)):
+            if items[index].position != index:
+                items[index].position = index
+                items[index].save()
 
     class Meta:
         abstract = True
